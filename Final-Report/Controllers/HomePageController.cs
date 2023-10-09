acebc13c7f5404544eaa73fa5a1b4b7ae93a7f71
Penlika <92129416+Penlika@users.noreply.github.com>
2023-10-09 23:46:02 +0700

    #

---
+++ Final-Report/Controllers/HomePageController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Final_Report.Controllers
+{
+    public class HomePageController : Controller
+    {
+        // GET: HomePage
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult NavBar()
+        { 
+            return PartialView(); 
+        }
+    }
+}
